b014297317692ea840a9510a824a09fc6e267fb7
Chris Grabosky <chris@grabosky.net>
2017-06-20 11:43:40 -0400

    Initial code

---
+++ SignEditor/SignEditor/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SignEditor
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ScreenManager _sm = new ScreenManager();
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btn_test_Click(object sender, RoutedEventArgs e)
+        { 
+            var val = await _sm.GetScreensAsync();
+            lv_main.ItemsSource = val;
+        }
+
+
+        private void lv_main_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Screen s = (Screen)e.ClickedItem;
+            txt_name.Text = s.name;
+            txt_feed.Text = s.feed;
+            txt_id.Text = s._id;
+            txt_duration.Text = s.duration;
+            txt_order.Text = s.order;
+            txt_sign_text.Text = s.sign_text;
+            txt_sign_type.Text = s.sign_type;
+            txt_uri.Text = s.uri;
+
+        }
+
+        private async void btn_update_Click(object sender, RoutedEventArgs e)
+        {
+            Screen s = new Screen();
+            s.name = txt_name.Text;
+            s.feed = txt_feed.Text;
+            s.duration = txt_duration.Text;
+            s.order = txt_order.Text;
+            s.sign_text = txt_sign_text.Text;
+            s.sign_type = txt_sign_type.Text;
+            s.uri = txt_uri.Text;
+            s._id = txt_id.Text;
+
+            await _sm.UpdateScreenAsync(s);
+
+            var val = await _sm.GetScreensAsync();
+            lv_main.ItemsSource = val;
+        }
+    }
+}
---
+++ SignEditor/SignEditor/ScreenManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace SignEditor
+{
+
+    public class Rootobject
+    {
+        public Screen[] Root { get; set; }
+    }
+
+    public class Screen
+    {
+        public string name { get; set; }
+        public string uri { get; set; }
+        public string feed { get; set; }
+        public string order { get; set; }
+        public string duration { get; set; }
+        public string sign_text { get; set; }
+        public string sign_type { get; set; }
+        public string _id { get; set; }
+        public int __v { get; set; }
+        public DateTime modified_date { get; set; }
+        public DateTime created_date { get; set; }
+    }
+
+    public class ScreenManager
+    {
+        
+
+        public async Task<Screen[]> GetScreensAsync()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://grabosky.net:8080/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Screen[] sl = null;
+            string items = "";
+
+
+            HttpResponseMessage response = await client.GetAsync("screens");
+            if (response.IsSuccessStatusCode)
+            {
+                items = await response.Content.ReadAsStringAsync();
+                sl = JsonConvert.DeserializeObject<Screen[]>(items);
+
+            }
+
+            return sl;
+        }
+
+        public async Task<Screen> GetScreenAsync(string id)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://grabosky.net:8080/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Screen s = null;
+            string item = "";
+
+
+            HttpResponseMessage response = await client.GetAsync("screens/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                item = await response.Content.ReadAsStringAsync();
+                s = JsonConvert.DeserializeObject<Screen>(item);
+
+            }
+
+            return s;
+        }
+
+        public async Task UpdateScreenAsync(Screen screenToUpdate)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string content = JsonConvert.SerializeObject(screenToUpdate);
+            string url = "screens/" + screenToUpdate._id;
+            var o = (Newtonsoft.Json.Linq.JObject)JsonConvert.DeserializeObject(content);
+            o.Property("_id").Remove();
+            o.Property("__v").Remove();
+            o.Property("modified_date").Remove();
+            o.Property("created_date").Remove();
+
+            StringContent sc = new StringContent(o.ToString(), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PutAsync(url, sc);
+            
+            response.EnsureSuccessStatusCode();
+
+
+            string r = await response.Content.ReadAsStringAsync();
+
+        }
+
+
+    }
+}
